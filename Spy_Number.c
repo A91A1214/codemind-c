0996813ac01c2f92fe060fb956a9f68f0837160f
A91A1214 <147997111+A91A1214@users.noreply.github.com>
2024-03-05 11:56:30 -0500

    #include<stdio.h> int main(){     int n,sum=0,pro=1,d;     scanf("%d",&n);     while(n>0){         d=n%10;         sum=sum+d;         pro=pro*d;         n=n/10;     }     if(sum==pro) printf("Spy Number");     else printf("Not Spy Number"); }

---
+++ Spy_Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0,pro=1,d;
+    scanf("%d",&n);
+    while(n>0){
+        d=n%10;
+        sum=sum+d;
+        pro=pro*d;
+        n=n/10;
+    }
+    if(sum==pro) printf("Spy Number");
+    else printf("Not Spy Number");
+}
